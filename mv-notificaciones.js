24c0ff769dde7c52eafdc6da23b2b22de4b69f95
meserovirtual <meserovirtualarg@gmail.com>
2017-02-22 18:17:46 -0200

    Mateo
    Crear el aviso para stock a reponer.

--- mv-notificaciones.js
+++ mv-notificaciones.js
@@ -22,34 +22,53 @@
         vm.aReponer = [];
         vm.avisosOriginales = [];
 
-        //console.log(UserService.getFromToken().data);
+        console.log(UserService.getFromToken().data);
 
         if(UserService.getFromToken().data != undefined) {
-            loadAvisos();
+            loadAReponer();
         }
 
-        function loadAvisos() {
-            console.log('cargar avisos');
+        function loadAReponer() {
 
             $interval(function () {
                 StockService.getAReponer(UserService.getFromToken().data.sucursal_id).then(function(data){
                     console.log(data);
-                    vm.aReponer = data;
+                    if(data.length > 0) {
+                        vm.aReponer = data;
+                        var aviso = {usuario_id: UserService.getFromToken().data.id, aviso: armarAvisoStockAReponer(data)};
+                        AvisosService.create(aviso).then(function(data){
+                            if(data > 0) {
+                                loadAvisos();
+                            }
+                        }).catch(function(data){
+                            console.log(data);
+                        })
+                    }
                 }).catch(function(data){
                     console.log(data);
                 });
-            }, 30000);
+            }, 300000);
 
         }
 
-        AvisosService.get().then(function (data) {
-            vm.avisos = data;
-            vm.avisosOriginales = [];
-            for(var i =0; i<data.length; i++){
-                vm.avisosOriginales.push(angular.copy(data[i]));
+        function armarAvisoStockAReponer(aReponer){
+            var aux = '';
+            for(var i = 0; i <= aReponer.length - 1; i++) {
+                aux += 'Produc:' + aReponer[i].producto_id + ' - ' + aReponer[i].nombre + ' - Cant Act:' + aReponer[i].cant_actual + ' - Pto Repo:' + aReponer[i].pto_repo + ' - ';
             }
-        });
+            return aux;
+        }
 
+        function loadAvisos() {
+            AvisosService.get().then(function (data) {
+                vm.avisos = data;
+                vm.avisosOriginales = [];
+                for(var i =0; i<data.length; i++){
+                    vm.avisosOriginales.push(angular.copy(data[i]));
+                }
+                console.log(vm.avisosOriginales);
+            });
+        }
 
     }
 
